b2aaa14caf4bb42975e5827faedd077be4a19272
Sharon Chen <sharonchen9593@gmail.com>
2018-11-07 23:52:46 -0800

    Matrix questions complete

--- Array and Strings/Questions/rotateMatrix.js
+++ Array and Strings/Questions/rotateMatrix.js
@@ -10,7 +10,25 @@
 const rotate = (mat) => {
     const layers = Math.floor(mat.length/2);
 
+    for(let x = 0; x < layers; x++) {
+        for(let y = 0; y < mat.length - 2; y++) {
+            const temp = mat[x][y]
+            const firstRotation = move(x, y, temp, mat);
+            const secondRotation = move(firstRotation.x, firstRotation.y, firstRotation.temp, mat)
+            const thirdRotation = move(secondRotation.x, secondRotation.y, secondRotation.temp, mat)
+            const fourthRotation = move(thirdRotation.x, thirdRotation.y, thirdRotation.temp, mat)
+        }
+    }
 
+    return mat;
+}
+
+const move = (x, y, temp, mat) => {
+    const newX = y;
+    const newY = mat.length - x -1;
+    const newTemp = mat[newX][newY];
+    mat[newX][newY] = temp;
+    return { x: newX, y: newY, temp: newTemp}
 }
 
 rotate(
@@ -21,3 +39,16 @@ rotate(
         [1, 2, 3, 4],
     ]
 )
+
+/**
+ (0, 0) => (0, 3), (0, 1) => (1, 3), (0, 2) => (2, 3), (0, 3) => (3, 3)
+ (1, 0) => (0, 2), (1, 1) => (1, 2), (1, 2) => (2, 2), (1, 3) => (3, 1)
+
+
+ (x, y)
+
+ x = y
+
+ y = length - x
+
+ **/
--- Array and Strings/Questions/zeroMatrix.js
+++ Array and Strings/Questions/zeroMatrix.js
@@ -2,7 +2,46 @@
  * If an element in an MxN matrix is 0, set the entire row and column to 0.
  *
  * Hints: 17, 74, 102
- * 
- * Time Complexity:
- * Space Complexity:
+ *
+ * Time Complexity: O(N);
+ * Space Complexity: O(1);
  **/
+
+const zeroMatrix = (mat) => {
+    const xZeros = {};
+    const yZeros = {};
+
+    for (let x = 0; x < mat.length ; x++) {
+        const row = mat[x];
+        for (let y = 0; y< row.length; y++) {
+            const num = row[y];
+            if (num === 0) {
+                xZeros[x] = true;
+                yZeros[y] = true;
+            }
+        }
+    }
+
+    for (let x = 0; x < mat.length ; x++) {
+        const row = mat[x];
+        for (let y = 0; y< row.length; y++) {
+            if(xZeros[x]) {
+                mat[x][y] = 0;
+            }
+            if (yZeros[y]) {
+                mat[x][y] = 0;
+            }
+        }
+    }
+
+    return mat
+}
+
+zeroMatrix(
+    [
+        [1, 1, 1, 0, 1],
+        [1, 0, 1, 1, 1],
+        [1, 0, 0, 1, 1],
+        [1, 1, 1, 1, 1]
+    ]
+)
