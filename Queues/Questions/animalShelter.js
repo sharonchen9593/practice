4821cfe62c372fa08af51d9bf7f9b9709d177db2
Sharon Chen <sharonchen9593@gmail.com>
2018-11-17 12:38:52 -0800

    Queues questions complete

--- Queues/Questions/animalShelter.js
+++ Queues/Questions/animalShelter.js
@@ -8,3 +8,91 @@
  * Time Complexity:
  * Space Complexity:
  **/
+
+class Animal {
+    constructor(name, order) {
+        this.name = name;
+        this.order = order;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    enQ(animal) {
+        if (!this.head) {
+            this.head = animal;
+            this.tail = animal;
+            return this.head;
+        }
+
+        this.tail.next = animal;
+        this.tail = animal;
+    }
+
+    deQ() {
+        const dequed = this.head;
+        this.head = this.head.next;
+        return dequed;
+    }
+
+    peek() {
+        return this.head;
+    }
+}
+
+class AnimalShelter {
+    constructor() {
+        this.cats = new Queue();
+        this.dogs = new Queue();
+        this.order = 0;
+    }
+
+    addCat(name) {
+        this.order = this.order + 1;
+        const animal = new Animal(name, this.order)
+        this.cats.enQ(animal);
+    }
+
+    addDog(name) {
+        this.order = this.order + 1;
+        const animal = new Animal(name, this.order)
+        this.dogs.enQ(animal);
+    }
+
+    removeAny() {
+        const cat = this.cats.peek();
+        const dog = this.dogs.peek();
+        if (cat && dog && cat.order < dog.order) {
+            return this.removeCat();
+        } else {
+            return this.removeDog();
+        }
+    }
+
+    removeCat() {
+        return this.cats.deQ().name;
+    }
+
+    removeDog() {
+        return this.dogs.deQ().name;
+    }
+}
+
+const x = new AnimalShelter();
+x.addCat('fluffy')
+x.addCat('lucky')
+x.addDog('mochi')
+x.addCat('tigger')
+x.addDog('ice')
+x.addDog('leo')
+
+x.removeAny()
+x.removeDog()
+x.removeCat()
+x.removeCat()
+x.removeAny()
--- Queues/Questions/queueWithStacks.js
+++ Queues/Questions/queueWithStacks.js
@@ -6,3 +6,65 @@
  * Time Complexity:
  * Space Complexity:
  **/
+
+class Stack {
+    constructor() {
+        this.list = [];
+    }
+
+    push(value) {
+        this.list.push();
+    }
+
+    pop() {
+        return this.list.pop();
+    }
+
+    peek() {
+        return this.list[this.list.length-1];
+    }
+
+    isEmpty() {
+        return this.list.length === 0;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.pushStack = new Stack();
+        this.popStack = new Stack();
+    }
+
+    push(value) {
+        this.pushStack.push(value);
+
+        if (this.popStack.isEmpty()) {
+            this.addValuesIntoPopStack();
+        }
+    }
+
+    addValuesIntoPopStack() {
+        while(!this.pushStack.isEmpty()) {
+            const popped = this.pushStack.pop();
+            this.popStack.push(popped);
+        }
+    }
+
+    pop() {
+        if (!this.popStack.isEmpty()) {
+            return this.popStack.pop();
+        } else {
+            this.addValuesIntoPopStack();
+            return this.popStack.pop();
+        }
+    }
+
+    peek() {
+        if (!this.popStack.isEmpty()) {
+            return this.popStack.peek();
+        } else {
+            this.addValuesIntoPopStack();
+            return this.popStack.peek();
+        }
+    }
+}
