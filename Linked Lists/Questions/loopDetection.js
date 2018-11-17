fbb682571652919a07b171e500bc4608af58b5be
Sharon Chen <sharonchen9593@gmail.com>
2018-11-17 11:48:30 -0800

    Linked list loop detection

--- Linked Lists/Questions/loopDetection.js
+++ Linked Lists/Questions/loopDetection.js
@@ -10,3 +10,77 @@
  * Time Complexity:
  * Space Complexity:
  **/
+
+class Node {
+    constructor(value, next) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    add(node) {
+        if (!this.head) {
+            this.head = node;
+            this.tail = node;
+            return this.head;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+        return this.head
+    }
+
+    loopTailTo(node) {
+        this.tail.next = node;
+    }
+}
+
+const x = new LinkedList();
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+const g = new Node('g');
+const h = new Node('h');
+const i = new Node('i');
+
+x.add(a)
+x.add(b)
+x.add(c)
+x.add(d)
+x.add(e)
+x.add(f)
+x.add(g)
+x.add(h)
+x.loopTailTo(d)
+
+const findLoopStart = (list) => {
+    let slow = list.head.next;
+    let fast = list.head.next.next;
+
+    while (slow !== fast) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    slow = list.head;
+
+    while (slow !== fast) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    console.log(slow)
+    return slow;
+}
+
+findLoopStart(x)
